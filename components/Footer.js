d5d716152c299e75203712fc4710bb77d0b6223e
Hassan Imran <hassan.imran7777@gmail.com>
2026-01-07 13:43:45 +0900

    Fix Footer.js syntax error

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
-export default function Footer {
+export default function Footer() {
   return (
     <footer className="bg-slate-900 text-white py-12 text-center">
-      <p>© 2026 OCTR Technologies. All rights reserved.</p>
+      <p>&copy; 2026 OCTR Technologies. All rights reserved.</p>
     </footer>
-  );
+  )
 }
